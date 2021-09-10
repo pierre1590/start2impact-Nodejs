00999de38c1f7b0178747bad9505fd286e5ac1b9
Piero Sabino <piero.sa@icloud.com>
2021-09-10 20:28:53 +0200

    update project

--- polluted_cities_places.sql
+++ polluted_cities_places.sql
@@ -29,7 +29,7 @@ CREATE TABLE `places` (
   `createdAt` datetime NOT NULL,
   `updatedAt` datetime NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=47 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
